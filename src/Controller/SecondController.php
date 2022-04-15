f13ba12e6c55a3edf15fc09bf3da7328e413fa65
aymen2782 <aymen.sellaouti@gmail.com>
2022-04-15 14:03:21 +0100

    router et twig

--- src/Controller/SecondController.php
+++ src/Controller/SecondController.php
@@ -7,15 +7,28 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/second')]
 class SecondController extends AbstractController
 {
-    #[Route('/second/{name}', name: 'app_second')]
-    public function index($name, Request $request): Response
+    #[Route(
+        '/{name}/{age<[01]?\d{1,2}>}',
+        name: 'app_second',
+//        requirements: ['age' => '[01]?\d{1,2}']
+//        defaults: ['name' => 'aymen']
+    )]
+    public function index($name, $age, Request $request): Response
     {
+        if ($age % 3 ===0) {
+            $this->addFlash('success', 'cc success');
+        }
+        if ($age % 2 ===0) {
+            $this->addFlash('info', 'cc info');
+        }
         dump($request);
         return $this->render('second/index.html.twig', [
             'controller_name' => 'SecondController',
             'esm' => $name,
+            'age' => $age
         ]);
     }
 }
